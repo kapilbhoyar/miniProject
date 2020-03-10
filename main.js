f26067f89a4ba8fb70c89a554b3be2fef0c65c5a
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-10 23:15:49 +0530

    condition

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 //const xlabels = [];
 //const ylabels = [];
+let ul = document.querySelector("ul");
 
-async function chartIt() {
-	const data = await getData();
-	console.log(data);
+async function chartIt(x, y) {
 	const ctx = document.querySelector("#chart").getContext("2d");
 	var myChart = new Chart(ctx, {
 		type: "bar",
 		data: {
-			labels: data.xs,
+			labels: x,
 			datasets: [
 				{
 					label: "# of Votes",
-					data: data.ys,
-					backgroundColor: "#fc97c8",
-					borderColor: "#ed1a80",
+					data: y,
+					backgroundColor: "#cf82ff",
+					borderColor: "#7300ba",
 					borderWidth: 1
 				}
 			]
@@ -33,27 +32,29 @@ async function chartIt() {
 	});
 }
 
-chartIt();
-
 //-----------------------------------------------------//
 
-async function getData() {
-	const xs = [];
-	const ys = [];
-	const response = await fetch("./csvjson.json");
-	const data = await response.json();
-	// const table = data.split("\n").slice(1);
-	data.forEach(row => {
-		const cop = row.CostofProductionC2;
-		const yield = row.Yield;
-		// if (row.Crop === "GRAM") {
-		// 	console.log("inside crop");
-			
-    // }
-    xs.push(cop);
-		ys.push(yield);
-	});
-	return { xs, ys };
+async function getData(event) {
+	if (event.target.tagName == "SPAN") {
+		var cropName = event.target.innerHTML;
+		const xs = [];
+		const ys = [];
+		const response = await fetch("./csvjson.json");
+		const data = await response.json();
+		// const table = data.split("\n").slice(1);
+		data.forEach(row => {
+			const cop = row.CostofProductionC2;
+			const yield = row.Yield;
+			if (row.Crop === cropName) {
+				console.log("inside crop");
+				xs.push(cop);
+				ys.push(yield);
+			}
+		});
+		chartIt(xs, ys);
+	}
 }
+
 //console.log(rows);
 // getData();
+ul.addEventListener("click", getData);
